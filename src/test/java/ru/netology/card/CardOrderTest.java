2a97a20c41c9e5ed105a5096280013700cf4d1a1
Анастасия Андрианова <samsikovaa@mail.ru>
2022-09-12 14:14:53 +0300

    8th commit

--- src/test/java/ru/netology/card/CardOrderTest.java
+++ src/test/java/ru/netology/card/CardOrderTest.java
@@ -1,61 +1,29 @@
 package ru.netology.card;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
 
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUp() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void setUp2() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
-    }
-
-    @AfterEach
-    public void close() {
-        driver.quit();
-        driver = null;
-    }
-
     @Test
-    public void shouldOpenBrowser() {
-        driver.findElement(By.cssSelector("[type=\"text\"]")).sendKeys("Иван Петров");
-        driver.findElement(By.cssSelector("[type=\"tel\"]")).sendKeys("+79563564112");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.tagName("button")).click();
+    public void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[action='/']");
+        $("[data-test-id=name] input").setValue("Петров Иван");
+        $("[data-test-id=phone] input").setValue("+79534658951");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[type=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
     }
 
-    @Test
-    public void shouldOpenBrowser2() {
-        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Петров");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79563564112");
-        driver.findElement(By.cssSelector("[data-test-id=agreement")).click();
-        driver.findElement(By.tagName("button")).click();
 
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-    }
 }
+
+
