47da1eed13b28341ce19be66e8bb68d32c3d1fb3
Анастасия Андрианова <samsikovaa@mail.ru>
2022-09-12 11:32:04 +0300

    fourth commit

--- src/test/java/ru/netology/card/CardOrderTest.java
+++ src/test/java/ru/netology/card/CardOrderTest.java
@@ -44,7 +44,6 @@ public class CardOrderTest {
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.tagName("button")).click();
 
-
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
@@ -55,7 +54,6 @@ public class CardOrderTest {
         driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79563564112");
         driver.findElement(By.cssSelector("[data-test-id=agreement")).click();
         driver.findElement(By.tagName("button")).click();
-
 
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
